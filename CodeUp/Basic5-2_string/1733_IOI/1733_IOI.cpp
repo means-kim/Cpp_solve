968c5be8ace882fd66420d86ed4e521ae1bef68b
mskim-99 <kms99400@gmail.com>
2026-07-14 17:41:09 +0900

    알파벳이 입력되었을 때 입력된 알파벳이 IOI인지 확인

---
+++ CodeUp/Basic5-2_string/1733_IOI/1733_IOI.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main()
+{
+  string s;
+
+  cin >> s;
+
+  if (s == "IOI")
+  {
+    cout << "IOI is the International Olympiad in Informatics.\n";
+  }
+  else
+  {
+    cout << "I don't care.\n";
+  }
+}
