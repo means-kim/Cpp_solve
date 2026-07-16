215e6eff7042767ebf93e0d47eaabe26d20f4e3a
mskim-99 <kms99400@gmail.com>
2026-07-16 22:58:39 +0900

    입력 받은 숫자만큼 hello를 출력

---
+++ CodeUp/Basic5-2_string/1735_hello/1735_hello.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main()
+{
+  int n;
+
+  cin >> n;
+  
+  // 입력받은 n만큼 "hello"를 출력
+  for (int i = 0; i < n; i++){
+    cout << "hello\n";
+  }
+}
